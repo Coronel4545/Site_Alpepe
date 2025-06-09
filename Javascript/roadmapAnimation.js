9bbb0c1e93f67e76567e9964632476e393927f94
Coronel4545 <stifler.oliveira41@gmail.com>
2025-06-09 11:24:37 -0300

    Delete Javascript directory

--- Javascript/roadmapAnimation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const roadmapItems = document.querySelectorAll('.roadmap-item');
-
-    roadmapItems.forEach((item) => {
-        item.addEventListener('click', () => {
-            item.classList.toggle('flipped'); // Alterna a classe "flipped" ao clicar
-        });
-    });
-});
